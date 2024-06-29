67e71a0504fa34837fd025d0be81d85700801895
aminbiography <biographyamin@gmail.com>
2024-06-29 17:38:50 +0600

    first commit

---
+++ index.py
@@ -0,0 +1,6 @@
+# Counting the number of characters in the provided string.
+text = "Mohammad Aminul Islam: Pioneering SEO Web Developer:"
+char_count = len(text)
+
+# Print the result
+print(f"The string contains {char_count} characters.")
